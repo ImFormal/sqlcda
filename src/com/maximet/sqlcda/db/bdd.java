258c8dd95ce7759aa5048d8bd4bb86975290b0e6
ImFormal <thubert.maxime.pro@gmail.com>
2025-02-26 10:08:08 +0100

    Bdd + connection

---
+++ src/com/maximet/sqlcda/db/bdd.java
@@ -0,0 +1,21 @@
+package com.maximet.sqlcda.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class bdd {
+    //Attribut paramètre BDD
+    static final String DB_URL = "jdbc:mysql://localhost/user_collection";
+    static final String USERNAME = "root";
+    static final String PASSWORD = "";
+    //Connexion à la BDD
+    private static Connection connexion;
+    static {
+        try {
+            connexion = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
